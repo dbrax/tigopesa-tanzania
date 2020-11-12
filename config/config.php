b7870ffe3eb3ca99982a4fdb130a7bd4fabf7935
emmanuel <emmanuel@emmanuels-MacBook-Air.local>
2020-11-12 11:46:02 +0300

    created a config file with all necessary parameters needed

--- config/config.php
+++ config/config.php
@@ -5,7 +5,21 @@
  */
 return [
 
+    'client_id'=>env("TIGO_CLIENT_ID"),
 
+
+    'secret'=>env("TIGO_CLIENT_SECRET"),
+
+    'api_url'=>env("TIGO_API_URL"),
     
+    'pin'=>env("TIGO_PIN"),
+
+    'account_number'=>env("TIGO_ACCOUNT_NUMBER"),
+    
+    'account_id'=>env("TIGO_ACCOUNT_ID")
+
+
 
 ];
+
+
