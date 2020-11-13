0b61e712d06983d9398df733705db5aed6155b04
emmanuel <emmanuel@emmanuels-MacBook-Air.local>
2020-11-13 16:07:53 +0300

    initial release 1.0.1 fixed providers and facades

--- config/config.php
+++ config/config.php
@@ -5,18 +5,68 @@
  */
 return [
 
+       /*
+     * Tigosecure Client id
+     */
+
     'client_id'=>env("TIGO_CLIENT_ID"),
 
+       /*
+     * Tigosecure  consumer secret
+     */
 
     'secret'=>env("TIGO_CLIENT_SECRET"),
 
+       /*
+     * Tigosecure  api  url
+     */
     'api_url'=>env("TIGO_API_URL"),
     
+       /*
+     * Tigosecure pin
+     */
     'pin'=>env("TIGO_PIN"),
 
+       /*
+     * Tigosecure  account number 
+     */
     'account_number'=>env("TIGO_ACCOUNT_NUMBER"),
     
-    'account_id'=>env("TIGO_ACCOUNT_ID")
+       /*
+     * Tigo secure  account id 
+     */
+    'account_id'=>env("TIGO_ACCOUNT_ID"),
+
+
+       /*
+     * Your  application url  
+     */
+    'app_url'=>env('APP_URL_LINK'),
+
+           /*
+     * Your  application  redirect url  
+     */
+    'redirect_url'=>env('TIGO_REDIRECT'),
+
+
+
+           /*
+     * Your  application  callback url  
+     */
+    'callback_url'=>env('TIGO_CALLBACK'),
+
+
+       /*
+     * Your  application currency code ( TZS ) for Tanzania  
+     */
+    'currency_code'=>env('APP_CURRENCY_CODE'),
+
+
+
+       /*
+     * Your  application language code en for english sw for swahili  
+     */
+    'lang'=>env('LANG')
 
 
 
--- src/Tigosecure.php
+++ src/Tigosecure.php
@@ -5,4 +5,50 @@ namespace Epmnzava\Tigosecure;
 class Tigosecure
 {
     // Build your next great package.
+
+
+    public  string $base_url,$issuedToken,$customer_firstname,$customer_lastname,$customer_email,$amount,$refecence_id;
+
+
+    public function access_token(){
+
+       $api=new TigoUtil();
+
+       $tokenArray=json_decode($api->get_access_token(config('tigosecure.api_url')));
+       $this->issuedToken=$tokenArray->accessToken;
+
+    }
+
+    public function make_payment($customer_firstname,$customer_lastname,$customer_email,$amount,$refecence_id){
+
+        $this->access_token();
+        $api=new TigoUtil();
+        $base_url=config('tigosecure.api_url');
+       $response=$api->makePaymentRequest($base_url,$this->issuedToken,$amount,$refecence_id,$customer_firstname,$customer_lastname,$customer_email);
+        
+       $array_response=json_decode($response);
+     
+       return $array_response;
+    }
+
+    /**
+     * @param string $prefix
+     * @param int $length
+     *
+     * @return string
+     */
+    public function random_reference($prefix = 'PESAPAL', $length = 15)
+    {
+        $keyspace = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        $str = '';
+
+        $max = mb_strlen($keyspace, '8bit') - 1;
+
+        for ($i = 0; $i < $length; ++$i) {
+            $str .= $keyspace[random_int(0, $max)];
+        }
+
+        return $prefix . $str;
+    }
 }
